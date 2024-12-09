d30eaf701c42935ee1828adef70d43ce0ce34039
nabilbilly <billsonb05@gmail.com>
2024-12-09 04:00:24 -0800

    New app created for teamworkspacechat and Employee

--- HiveProject/TeamworkspaceChat/urls.py
+++ HiveProject/TeamworkspaceChat/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
-    path("", chat_views.chatPage, name="chat-page"),
-
+    # path("", chat_views.chatPage, name="chat-page"),
+    path("team/<slug:team_slug>/", chat_views.chatPage, name="chat-page"),
     # Custom login view
     path("auth/logout/", LogoutView.as_view(), name="logout-user"),
 ]
--- HiveProject/TeamworkspaceChat/views.py
+++ HiveProject/TeamworkspaceChat/views.py
@@ -7,11 +7,25 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from .models import Message
 
-# Create your views here.
-def chatPage(request, *args, **kwargs):
-    if not request.user.is_authenticated:
+# # Create your views here.
+# def chatPage(request, *args, **kwargs):
+#     if not request.user.is_authenticated:
+#         return redirect("login-user")
+#     # Retrieve the last 50 messages and reverse the queryset
+#     messages = Message.objects.order_by('timestamp')[:50]
+#     context = {"messages": messages}
+#     return render(request, "Teamworkspace/chatPage.html", context)
+
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from Teamworkspace.models import Team, TeamMembership
+
+@login_required
+def chatPage(request, team_slug):
+    team = get_object_or_404(Team, slug=team_slug)
+    if not TeamMembership.objects.filter(team=team, user=request.user).exists():
         return redirect("login-user")
-    # Retrieve the last 50 messages and reverse the queryset
-    messages = Message.objects.order_by('timestamp')[:50]
-    context = {"messages": messages}
-    return render(request, "Teamworkspace/chatPage.html", context)
+    messages = team.messages.order_by('timestamp')[:50]
+    context = {"team": team, "messages": messages}
+    return render(request, "Teamworkspace/TeamworkspaceChat.html", context)
